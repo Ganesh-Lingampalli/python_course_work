92055a9e7f86e4102ee447c8a3b0b93893962a66
Ganesh-lingampalli <lingampalliganesh99@gmail.com>
2026-09-05 15:52:21 +0530

    User form validation and text data processing using regex

---
+++ Day-35/formvalidation.py
@@ -0,0 +1,52 @@
+import re
+
+'''Validation of names'''
+
+# fullname = input("Enter the fullname: ")
+# pattern = r'^[A-Za-z]{2,25}( [A-Za-z]{2,25})+$'
+# res = re.fullmatch(pattern,fullname)
+
+# print("Valid fullname" if res else "invalid fullname")
+
+'''Email validation'''
+
+# email = input("Enter the email: ")
+# pattern = r'^[A-za-z._0-9]+@[A-Za-z0-9._]+\.[a-zA-z]{2,}$'
+# res = re.fullmatch(pattern,email)
+# print("Valid email" if res else "Invalid email")
+
+'''Phone number validation'''
+
+# phone_num = input('Enter the phone number: ')
+# pattern = r'^(?:\+91|0)?[6-9]\d{9}$'
+# res = re.fullmatch(pattern,phone_num)
+
+# print("Valid Phone number" if res else "Invalid Phone number")
+
+'''Paaword validation'''
+
+# password = input("Enter the password: ")
+# pattern = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+# res = re.fullmatch(pattern,password)
+
+# print("Valid Password" if res else "Invalid Password")
+
+'''Instagram usename validation'''
+
+# username = input("Enter the username: ")
+# pattern = r"^[A-za-z0-9._]+$"
+# res = re.fullmatch(pattern,username)
+
+# print("Valid" if res else "Invalid")
+
+'''Aadhar number validation'''
+
+# Aadhar_num = input("Enter Aadhar number: ")
+# pattern = r"^[3-9]\d{11}$"
+# res = re.fullmatch(pattern,Aadhar_num)
+
+# print("Valid" if res else "Invalid")
+
+'''Pancard validation'''
+
+Pancard = input("Enter pancard number: ")
